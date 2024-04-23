eca27094e7e7636b65c4b0c5dc4c4acdc0e664f2
Tuoi Tho da Khoc <135036590+tuoitho@users.noreply.github.com>
2024-04-23 20:17:32 +0700

    Update nhan_dang_trai_cay.py

--- nhan_dang_trai_cay.py
+++ nhan_dang_trai_cay.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import cv2
-st.title('Nhận dạng trái cây')
+st.title('Nhận dạng trái câyy')
 
 try:
     if st.session_state["LoadModel"] == True:
